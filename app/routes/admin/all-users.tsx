1a20a885007ad2cdab49248acb379418d75f8b1f
Hemant Verma <vermahemant837@gmail.com>
2025-05-20 23:08:12 +0530

    cards added

--- app/routes/admin/all-users.tsx
+++ app/routes/admin/all-users.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import { Header } from "components";
 
 const AllUsers = () => {
-  return <div>AllUsers</div>;
+  return (
+    <main className="dashboard wrapper">
+      <Header
+        title="Trips page"
+        description="Check out our current users in real time"
+      />
+      All Users Page Contents
+    </main>
+  );
 };
 
 export default AllUsers;
